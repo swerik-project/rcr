c481f87be6c088e51f1bae23254eef9097e25cd8
Mans Magnusson <mons.magnusson@gmail.com>
2023-12-13 10:59:46 +0100

    Added extract speeches

---
+++ R/extract_speeches.R
@@ -0,0 +1,57 @@
+#' Extract Speeches from Records
+#'
+#' @description
+#' The function extract speeches from the Riksdagen Records based on the
+#' definition of a speech as the utterances (<u>) coming after a speaker
+#' introduction (<note type="speaker">). The function returns the segments
+#' of the speech.
+#'
+#' For multiple files, parallelism can be used.
+#'
+#' @param record_path a file path to a record XML file
+#' @param record_paths a vector of file paths to record XML files
+#'
+#'
+#' @return
+#' The function returns a tibble data frame with the following variables:
+#' \describe{
+#'   \item{speech_no}{The speech number in the record.}
+#'   \item{speech_id}{The id of the XML node to the introduction of the speaker.}
+#'   \item{who}{The id of the person giving the speech.}
+#'   \item{id}{The id of the XML node for the segment of the speech.}
+#'   \item{text}{The speech segment as plain text.}
+#' }
+#'
+#'
+#' @export
+extract_speeches_from_record <- function(record_path){
+  # Assert the file exists
+  checkmate::assert_string(record_path)
+  rcp <- get_riksdag_corpora_path()
+  rcfp <- file.path(rcp, record_path)
+  if(file.exists(rcfp)){
+    record_paths <- rcfp
+  }
+  checkmate::assert_file_exists(record_path)
+
+  x <- xml2::read_xml(record_path)
+  x <- xml_ns_strip(x)
+
+  # Extract speeches
+  xs <- xml2::xml_find_all(x, ".//note[@type = 'speaker']|.//u|.//seg")
+  df <- dplyr::tibble(type_speaker = xml_attr(xs, attr = "type") == "speaker",
+                      name = xml_name(xs),
+                      who = xml_attr(xs, attr = "who"),
+                      id = xml_attr(xs, attr = "id"),
+                      text = xml_text(xs, trim = TRUE))
+  df$type_speaker[is.na(df$type_speaker)] <- FALSE
+  df$speech_no <- cumsum(df$type_speaker)
+  df$speech_id <- df$id
+  df$speech_id[!df$type_speaker] <- NA
+  df <- tidyr::fill(df, who, speech_id)
+  df <- df[df$name == "seg",]
+  df$type_speaker <- NULL
+  df$name <- NULL
+  df[, c("speech_no", "speech_id", "who", "id", "text")]
+}
+
---
+++ tests/testthat/test-extract_speeches.R
@@ -0,0 +1,19 @@
+test_that("extracting speeches works", {
+  if(FALSE){
+    # Setup testdata
+    tdfp <-
+      c("protocols/1896/prot-1896--ak--042.xml",
+        "protocols/1951/prot-1951--fk--029.xml",
+        "protocols/1975/prot-1975--036.xml")
+    tdfpf <- file.path(get_riksdag_corpora_path(), tdfp)
+    tdfpt <- file.path("tests/testthat/files/", basename(tdfp))
+    file.copy(tdfpf, tdfpt)
+    cat(paste0("tfp <- c('", paste(basename(tdfp),collapse =  "', '"), "')"))
+  }
+
+  tfp <- c('prot-1896--ak--042.xml', 'prot-1951--fk--029.xml', 'prot-1975--036.xml')
+  tfp <- test_path(file.path("files", tfp))
+
+  expect_silent(sp <- extract_speeches_from_record(tfp[1]))
+  expect_error(sp <- extract_speeches_from_record(tfp))
+})
